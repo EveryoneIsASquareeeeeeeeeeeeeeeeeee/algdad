76884b31362c62f324ea5ffa5c087fb471c20240
EveryoneIsASquareeeeeeeeeeeeeeeeeee <elitehammerhead123@gmail.com>
2023-03-15 23:53:43 +1030

    Uploading final changes to reverser header

--- assignment3/Reverser.cpp
+++ assignment3/Reverser.cpp
@@ -3,10 +3,16 @@
 int Reverser::reverseDigit(int value)
 {
     int revValue, remainder;
-    while (value != 0){
-        remainder = value % 10;
-        revValue = revValue * 10 + remainder;
-        value /= 10;
+    if (value <= 0)
+    {
+        return -1;
+    }else{
+        while (value != 0)
+        {
+            remainder = value % 10;
+            revValue = revValue * 10 + remainder;
+            value /= 10;
+        }
     };
     return revValue;
 };
@@ -14,9 +20,13 @@ int Reverser::reverseDigit(int value)
 std::string Reverser::reverseString(std::string characters)
 {
     std::string revChar;
-
-    for(int i = characters.length()-1; i >= 0; i--){
-        revChar.push_back(characters[i]);
+    if (characters == "")
+    {
+        return "ERROR";
+    }else{
+        for(int i = characters.length()-1; i >= 0; i--){
+            revChar.push_back(characters[i]);
+        };
     };
     return revChar;
 };
--- assignment3/main.cpp
+++ assignment3/main.cpp
@@ -6,6 +6,6 @@ int main(){
     Truckloads tr;
     std::cout << tr.numTrucks(1024,5) << std::endl;
     Reverser r;
-    std::cout << r.reverseDigit(12345) << std::endl;
+    std::cout << r.reverseDigit(0) << std::endl;
     std::cout << r.reverseString("Hello") << std::endl;
 };
